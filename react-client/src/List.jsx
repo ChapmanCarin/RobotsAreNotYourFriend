2b8fea65831349f8f867aac2f2f4025b55539dc0
Carin Chapman <carinchapman@Carins-MacBook-Pro.local>
2019-12-06 09:29:20 -0600

    Set unique key for robot

--- react-client/src/List.jsx
+++ react-client/src/List.jsx
@@ -3,9 +3,9 @@ import ListItem from './ListItem.jsx';
 
 
 const List = ({ robots }) => (
-  <div>
-    {robots.map(robot => <ListItem robot={robot} />)}
-  </div>
+<div>
+    {robots.map(robot => <ListItem robot={robot} key={robot.name}/>)}
+</div>
 )
 
 export default List;
